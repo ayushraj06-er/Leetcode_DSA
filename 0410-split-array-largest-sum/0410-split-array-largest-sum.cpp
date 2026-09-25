62c16e570897bd55ed92523cad9af7ed1dec5d35
Ayush Raj <ayushrajtelghi55@gmail.com>
2026-09-25 19:38:13 +0530

    Time: 0 ms (100%), Space: 10.5 MB (73.38%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int returnSum(vector<int>& arr,int n){
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    int cntSubArr(vector<int>& arr ,int n,int mid){
+        int cnt=0;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            if(sum+arr[i]<=mid){
+                sum+=arr[i];
+            }else{
+                cnt++;
+                sum=arr[i];
+            }
+        }
+        if(sum<=mid){
+            cnt++;
+        }
+        return cnt;
+    }
+    int splitArray(vector<int>& arr, int k) {
+        int n=arr.size();
+        int st=*max_element(arr.begin(),arr.end());
+        int end=returnSum(arr,n);
+        while(st<=end){
+            int mid=(st+end)/2;
+            int subArr=cntSubArr(arr,n,mid);
+            if(subArr<=k){
+                end=mid-1;
+            }else{
+                st=mid+1;
+            }
+
+        }
+        return st;
+
+    }
+};
